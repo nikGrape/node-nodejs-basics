d5806c6c7e81e116c2b120c097adc755304da147
Nikolai Vinogradov <nik@Nikolais-MBP.hsd1.ca.comcast.net>
2023-06-18 13:02:12 -0700

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const script_path = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	// Write your code here
+	const childProcess = spawn('node', [script_path, ...args], {
+		stdio: ['pipe', 'pipe', process.stderr],
+	});
+	process.stdin.pipe(childProcess.stdin);
+	childProcess.stdout.on('data', (data) => {
+		process.stdout.write(data);
+	});
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
