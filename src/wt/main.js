2b73afd54e7e52bf91f273ffb734342eec49ca2b
Nikolai Vinogradov <nik@Nikolais-MBP.hsd1.ca.comcast.net>
2023-06-18 12:38:18 -0700

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import os from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __worker_filename = path.join(__dirname, 'worker.js');
+const CPUs = os.cpus();
+
 const performCalculations = async () => {
-    // Write your code here
+	// Write your code here
+	const workers = Array.from(
+		{ length: CPUs.length },
+		(_, i) =>
+			new Worker(__worker_filename, {
+				workerData: { n: 10 + i },
+			})
+	);
+
+	const promises = workers.map((worker) => {
+		return new Promise((resolve, _) => {
+			worker
+				.on('message', (data) => {
+					resolve({ status: 'resolved', data });
+				})
+				.on('error', (err) => {
+					resolve({ status: 'error', data: null });
+				});
+		});
+	});
+
+	const results = (await Promise.allSettled(promises)).map((v) => v.value);
+
+	console.log(results);
 };
 
 await performCalculations();
